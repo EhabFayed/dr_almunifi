c05182a1927d943fca7533783ab8513f326d68e6
ehab fayed <efayed@damlag.com>
2026-01-17 22:53:20 +0200

    prod config was missing "

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = {
-    host: ENV.fetch("RAILS_HOST", "backend.almunifi.com),
+    host: ENV.fetch("RAILS_HOST", "backend.almunifi.com"),
     protocol: ENV.fetch("RAILS_PROTOCOL", "https")
   }
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
